f35e64c5daf5c1fb209f09e2384dbc53f09d2003
I-AnjaliKushwaha <anjalikushwaha658@gmail.com>
2024-02-16 11:29:33 +0530

    signup and signin

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,6 @@
 const { catchASyncErrors } = require("../middlewares/catchAsyncErrors");
 const Student = require("../models/studentModel");
+const ErrorHandler = require("../utils/ErrorHandler");
 
 exports.homepage = catchASyncErrors(async(req, res, next) => {
             res.json({message: "homepage"});           
@@ -14,9 +15,20 @@ exports.studentsignin = catchASyncErrors(async(req, res, next) => {
             const student = await Student.findOne({ email: req.body.email })
             .select("+password")
             .exec();
+
+            if(!student) return next(new ErrorHandler("User not found with this email address", 404));
+
+             // compare the password of the user who is trying to login and hashed password in
+             // database using bcrypt then check if they match or not
+
+             const isMatch = student.comparepassword(req.body.password);
+             if(!isMatch) return next(new ErrorHandler("Wrong Password", 500));
+
+            
+
             res.json(student);
 });  
 
 
 
-exports.studentsignout = catchASyncErrors(async(req, res, next) => {});  
+exports.studentsignout = catchASyncErrors(async(req, res, next) => {});    

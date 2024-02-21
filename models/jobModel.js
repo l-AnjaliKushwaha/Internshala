f2b2ac17eaacd256ba171caed05789ce1fccbfde
I-AnjaliKushwaha <anjalikushwaha658@gmail.com>
2024-02-21 11:29:15 +0530

    jobModel done

---
+++ models/jobModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const jobModel = new mongoose.Schema(
+            {
+                        title: String,
+                        skill: String,
+                        jobtype: { type: String, enum: ["In office", "Remote"] },
+                        openings: Number,
+                        description: String,
+                        preferences: String,
+                        salary: Number,
+                        perks: String,
+                        assesments: String,
+            },
+            { timestamps: true }
+);
+
+
+const Job = mongoose.model("job", jobModel);
+
+module.exports = Job;

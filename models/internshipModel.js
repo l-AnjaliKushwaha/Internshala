0f5da2c710dbd3ab3933ba25a43a38958747a4d4
I-AnjaliKushwaha <anjalikushwaha658@gmail.com>
2024-02-21 11:23:13 +0530

    internshipModel Done

--- models/internshipModel.js
+++ models/internshipModel.js
@@ -3,22 +3,27 @@ const mongoose = require("mongoose");
 const internshipModel = new mongoose.Schema(
             {
                         profile: String,
+                        skill: String,
                         internshiptype: { type: String, enum: ["In office", "Remote"]},
+                        openings: Number,
+                        from: String,
+                        to: String,
+                        duration: String,
+                        responsibility: String,
+                        stipend: {
+                                    status: {
+                                                type: String,
+                                                enum: ["Fixed", "Negotiable", "Performance based", "Unpaid"],
+                                    },
+                                    amount: Number,
+                        },
+                        perks: String,
+                        assesments: String,         
             },
             { timestamps: true }
 );
 
-internshipModel.pre("save", function () {
-            if (!this.isModified("password")) {
-                        return;
-            }
-            let salt = bcrypt.genSaltSync(10);
-            this.password = bcrypt.hashSync(this.password, salt);
-});
 
-
-
-
-const Internship = mongoose.model("Internship", internshipModel);
+const Internship = mongoose.model("internship", internshipModel);
 
 module.exports = Internship;
